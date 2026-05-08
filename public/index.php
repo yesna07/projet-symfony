15ed88885ba5d8345b53247767df2e201979ca0b
yesna07 <seynabou.naby.ndaw@etu.univ-st-etienne.fr>
2026-05-08 23:01:54 +0200

    Mise à jour du design violet et correction de la navbar

--- public/index.php
+++ public/index.php
@@ -111,17 +111,15 @@
     </div>
 
     <!-- Menu fixe -->
+    
     <nav class="navbar navbar-expand-lg fixed-top py-3 shadow-sm">
         <div class="container">
             <a class="navbar-brand fw-bold" href="index.php">Welcome</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-            </div>
         </div>
     </nav>
-
     <!-- Hero Section -->
     <section class="hero pt-5 mt-5">
         <div class="container">
